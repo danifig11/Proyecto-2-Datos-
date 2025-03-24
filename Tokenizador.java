6f8abc6acc571ff16335588ab736d49e05231669
llAkihitoll <saul.esteban.casti@gmail.com>
2025-03-24 12:39:56 -0600

    Agregar el patron para agregar los elementos a lisp

--- Tokenizador.java
+++ Tokenizador.java
@@ -1,17 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+/*
+ * UNIVERSIDAD DEL VALLE DE GUATEMALA
+ * Departamento de Ciencia de la Computación
+ * Autores: 
+ *  - Daniel Figueroa 24073
+ *  - Oliver Cifuentes 241021
+ *  - Saul Castillo 24915
+ * Fecha: Marzo 2025
+ * 
+ * Tokenizador: Clase encargada de dividir una expresión en tokens para su procesamiento.
+ */
 
-public class Tokenizador {
-    public static List<String> dividirEnTokens(String entrada) {
-        List<String> tokens = new ArrayList<>();
-        Pattern patron = Pattern.compile("\\(|\\)|[a-zA-Z0-9+\\-*/=<>]+|'");
-        Matcher coincidencia = patron.matcher(entrada);
-
-        while (coincidencia.find()) {
-            tokens.add(coincidencia.group());
-        }
-        return tokens;
-    }
-}
+ import java.util.ArrayList;
+ import java.util.List;
+ import java.util.regex.Matcher;
+ import java.util.regex.Pattern;
+ 
+ public class Tokenizador {
+     public static List<String> dividirEnTokens(String entrada) {
+         List<String> tokens = new ArrayList<>();
+         Pattern patron = Pattern.compile("\\(|\\)|'?[a-zA-Z0-9+\\-*/=<>.]+|'");
+         Matcher coincidencia = patron.matcher(entrada);
