03266e8f2d86e53108285178c43bb0cafca429a6
llAkihitoll <saul.esteban.casti@gmail.com>
2025-03-24 12:57:25 -0600

    procesar tokens, conversion y evaluacion

--- Main.java
+++ Main.java
@@ -15,9 +15,21 @@
 
  import java.util.*;
 
- public class Main {
-     public static void main(String[] args) {
-         try (Scanner escaner = new Scanner(System.in)) {
-             System.out.println("Ingresa una expresión en LISP:");
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner escaner = new Scanner(System.in)) {
+            System.out.println("Ingresa una expresión en LISP:");
 
-             
+            String entrada = escaner.nextLine();
+            List<String> tokens = Tokenizador.dividirEnTokens(entrada);
+            System.out.println("Tokens: " + tokens);
+
+            Object expresionConvertida = Convertir.convertir(tokens);
+            Object resultado = Evaluador.evaluar(expresionConvertida);
+
+            if (resultado != null) {
+                System.out.println("Resultado: " + resultado);
+            }
+        }
+    }
+}
